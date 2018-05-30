db018e81d974799774e00c9665c17df009d1342a
Dpopov92 <denpopov192@gmail.com>
2018-05-30 17:34:50 +0300

    #first commit#

--- TestClass.java
+++ TestClass.java
@@ -8,22 +8,23 @@ import org.openqa.selenium.support.PageFactory;
 import java.util.concurrent.TimeUnit;
 
 public class TestClass {
+    static LoginPage loginPage;
+    static GmailPage gmailPage;
+    static LetterPage letterPage;
     private static WebDriver driver;
-    private String email = "lolmage92@gmail.com";
-    private String pass = "476159831asd";
+    private String email = "test@gmail.com";
+    private String pass = "test";
     private String url = "http://www.gmail.com";
 
-    LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
-    GmailPage gmailPage = PageFactory.initElements(driver, GmailPage.class);
-    LetterPage letterPage = PageFactory.initElements(driver, LetterPage.class);
-
     @BeforeClass
-    public static void initialDriver() {
+    public static void initial() {
         System.setProperty("webdriver.gecko.driver", "C:/Program Files/geckodriver-v0.20.1-win64/geckodriver.exe");
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        loginPage = PageFactory.initElements(driver, LoginPage.class);
+        gmailPage = PageFactory.initElements(driver, GmailPage.class);
+        letterPage = PageFactory.initElements(driver, LetterPage.class);
     }
-
 
 
     @Test
@@ -33,9 +34,9 @@ public class TestClass {
         gmailPage.sendMessage(email);
         driver.quit();
 
-        initialDriver();
-        loginPage.open(driver,url);
-        loginPage.login(driver,email,pass);
+        initial();
+        loginPage.open(driver, url);
+        loginPage.login(driver, email, pass);
         gmailPage.checkEmail(driver);
         Assert.assertEquals(email, letterPage.getFirstEmail());
 
